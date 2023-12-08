c756fecd094963bb2d5aa81f5d14c93b4917d57d
athotabharat <bharat_athota@bio-rad.com>
2023-12-08 18:37:01 +0530

    assignment-two

--- node-mongo-api/routes/issueRoutes.js
+++ node-mongo-api/routes/issueRoutes.js
@@ -14,8 +14,75 @@ router.post('/', async (req, res) => {
 
 router.get('/', async (req, res) => {
   try {
+    const page = req.query.page;
+    const pageSize = 5;
     const issues = await Issues.find();
-    res.send(issues);
+    const startIndex = (page - 1) * pageSize;
+    const endIndex = startIndex + pageSize;
+    if(req.query.page) {
+      const pageData = issues.slice(startIndex, endIndex);
+      res.send(pageData);
+    } else {
+      res.send(issues);
+    }
+   
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const issue = new Issues(req.body);
+    await issue.save();
+    res.status(200).send(issue);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.get('/:index', async (req, res) => {
+  try {
+    const issue = await Issues.find({ index: req.params.index });
+    if (!issue) {
+      return res.status(404).send();
+    }
+    res.send(issue);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.put('/:index', async (req, res) => {
+  try {
+    const filter = { index: req.params.index };
+    if(req.body.upvotes) {
+      const count = req.body.upvotes;
+      const issue = await Issues.updateOne(filter, { $set: { upvotes: count } });
+        if (!issue) {
+          return res.status(404).send();
+        }
+        res.send(issue);
+    } else {
+      const issue = await Issues.updateOne(filter, { $set: { comments: req.body } });
+        if (!issue) {
+          return res.status(404).send();
+        }
+        res.send(issue);
+    }
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+
+router.delete('/:index', async (req, res) => {
+  try {
+    const issue = await Issues.deleteOne({index: req.params.index});
+    if (!issue) {
+      return res.status(404).send();
+    }
+    res.send(issue);
   } catch (error) {
     res.status(500).send(error);
   }
